da320a7551aba3f63f113af5eacb45dfa29871e1
tjmeltesen <tjmeltesen@gmail.com>
2026-06-19 15:45:59 -0500

    registry update

--- subnet_broker/broker_main.lua
+++ subnet_broker/broker_main.lua
@@ -7,7 +7,7 @@
     loadfile("/home/subnet_broker/broker_main.lua")("test")  -- one tick, then exit
 ]]
 
-local BROKER_BUILD = "2026-06-19-me-onlyv2"
+local BROKER_BUILD = "2026-06-19-me-onlyv3"
 
 local sep = package.config:sub(1, 1)
 local here = (arg and arg[0] and arg[0]:match("^(.*)[/\\]")) or "/home/subnet_broker"
