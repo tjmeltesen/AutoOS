236526e4a266fcb33c201b1b6b8cf2ea913c8d8e
tjmeltesen <tjmeltesen@gmail.com>
2026-06-21 19:37:04 -0500

    maxing log file and trying to fix bug

--- subnet_broker/tasks/task_lane_worker.lua
+++ subnet_broker/tasks/task_lane_worker.lua
@@ -22,17 +22,19 @@ function Task.spawn(ctx, machine)
     while true do
       wake_count = wake_count + 1
       local job = rob:get_assigned_job(machine.id)
-      -- Heartbeat every 20 wakes so we know the lane is alive
-      if wake_count % 20 == 1 then
+      -- Log every wake until we get a job, then go quiet
+      if not job then
         local lane_state = "?"
         if rob._lanes and rob._lanes[machine.id] then
           lane_state = rob._lanes[machine.id].state or "?"
         end
-        log(string.format("[LaneTask] %s wake=%d state=%s has_job=%s",
-          machine.id, wake_count, lane_state, tostring(job ~= nil)))
+        if wake_count <= 5 or wake_count % 20 == 1 then
+          log(string.format("[LaneTask] %s wake=%d state=%s NO_JOB",
+            machine.id, wake_count, lane_state))
+        end
       end
       if job and LaneWorker then
-        log(string.format("[LaneTask] %s executing job %s", machine.id, job.id))
+        log(string.format("[LaneTask] %s wake=%d GOT_JOB %s", machine.id, wake_count, job.id))
         local ok_exec, result = pcall(LaneWorker.execute, registry, job, machine.id)
         if not ok_exec then
           result = { status = "failed", error = "LaneWorker crashed: " .. tostring(result) }
