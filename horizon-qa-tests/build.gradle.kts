045b50a88562046df091e6339191d7516854c3f9
tjmeltesen <tjmeltesen@gmail.com>
2026-06-25 00:25:14 -0500

    final push of horizon-qa

--- horizon-qa-tests/build.gradle.kts
+++ horizon-qa-tests/build.gradle.kts
@@ -1,32 +1,11 @@
 plugins {
-    id 'com.gtnewhorizons.retrofuturagradle' version '1.3.35'
+    id("com.gtnewhorizons.gtnhconvention")
 }
 
-group = 'autos'
-version = '1.0.0'
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(8)
-    }
+// Configure Javadoc to prevent build failures
+tasks.withType<Javadoc>().configureEach {
+    val javadocOptions = options as StandardJavadocDocletOptions
+    javadocOptions.addStringOption("Xdoclint:none", "-quiet")
+    javadocOptions.encoding = "UTF-8"
+    javadocOptions.charSet = "UTF-8"
 }
-
-minecraft {
-    mcVersion = '1.7.10'
-    username = 'AutoOS-QA'
-}
-
-repositories {
-    maven {
-        name = 'GTNH Maven'
-        url = 'https://nexus.gtnewhorizons.com/repository/public/'
-    }
-    mavenCentral()
-}
-
-dependencies {
-    // Horizon-QA GameTest framework
-    testImplementation 'com.gtnewhorizons:Horizon-QA:1.0.+:dev'
-}
-
-// Horizon-QA CI mode: -Dhorizonqa.mode=ci passed via --mcJvmArgs in workflow
